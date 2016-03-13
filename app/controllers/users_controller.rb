5aad343ddcf53fb4bd415b6eb517926fc32a9eca
herokato <h_htb_atm_k@yahoo.co.jp>
2016-03-13 21:01:55 +0000

    7.6 2nd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -18,8 +19,24 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Complete editting your profile."
+      redirect_to user_path
+    else
+      render 'edit'
+    end
+  end
+  
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
