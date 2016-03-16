4a25f697aec7ead5d2cd7eb13d0781fe9378dfc0
herokato <h_htb_atm_k@yahoo.co.jp>
2016-03-16 06:45:09 +0000

    add followings-followers page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user, only:[:edit, :update]
+  
+  def index
+    @users = User.all
+  end
   
   def show
     @user = User.find(params[:id])
@@ -31,6 +35,20 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following_users
+    render 'show_follow'
+  end
+  
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.follower_users
+    render 'show_follow'
   end
   
   private
