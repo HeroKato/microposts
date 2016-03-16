340a787ce1f6390c7fa39b1072e3324928062177
herokato <h_htb_atm_k@yahoo.co.jp>
2016-03-16 17:14:49 +0000

    add profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user, only:[:edit, :update]
+  before_action :user_find, only:[:show, :create, :following, :followers]
   
   def index
     @users = User.all
   end
   
   def show
-    @user = User.find(params[:id])
     @microposts = @user.microposts.order(created_at: :desc)
   end
   
@@ -16,7 +16,6 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
@@ -39,14 +38,12 @@ class UsersController < ApplicationController
   
   def following
     @title = "Following"
-    @user = User.find(params[:id])
     @users = @user.following_users
     render 'show_follow'
   end
   
   def followers
     @title = "Followers"
-    @user = User.find(params[:id])
     @users = @user.follower_users
     render 'show_follow'
   end
